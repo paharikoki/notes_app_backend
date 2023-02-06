fb55df7eb9685b6949d18c6e63d541e9cb67c890
Ahmad Alan Lestari <paharikoki@gmail.com>
2023-02-06 14:21:04 +0700

    Initial commit

---
+++ src/notes.js
@@ -0,0 +1,38 @@
+const notes =[
+    {
+        "title": "Catatan A",
+        "tags": [
+            "Android",
+            "Web"
+        ],
+        "body": "Isi dari catatan A",
+        "id": "A5HC13PSGynpA9dI",
+        "createdAt": "2023-02-06T07:14:00.113Z",
+        "updatedAt": "2023-02-06T07:14:00.113Z"
+    },
+    {
+        "title": "Catatan B",
+        "tags": [
+            "Android",
+            "Web",
+            "Back-End"
+        ],
+        "body": "Isi dari catatan B",
+        "id": "2MJA65qkKUTwFR1J",
+        "createdAt": "2023-02-06T07:14:13.540Z",
+        "updatedAt": "2023-02-06T07:14:13.540Z"
+    },
+    {
+        "title": "Catatan C",
+        "tags": [
+            "Android",
+            "Back-End"
+        ],
+        "body": "Isi dari catatan C",
+        "id": "svh5aUI-xCnGDDwY",
+        "createdAt": "2023-02-06T07:14:22.391Z",
+        "updatedAt": "2023-02-06T07:14:22.391Z"
+    }
+];
+
+module.exports = notes;
